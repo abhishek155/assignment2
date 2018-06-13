75478ba0e0506ee5e1ed5fc1b14a83064042c08b
abhishek155 <abhishek.kashyap1443@gmail.com>
2018-06-13 15:31:17 +0530

    assignment2

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+# Ques 1-
+print("acadview")
+
+#Ques 2-
+print ("acad" + "view")
+
+#Ques 3-
+x = 1
+y = 2
+z = 3
+print(x,y,z)
+
+#Ques 4-
+print ('let\'s get started')
+
+#Ques 5-
+s="acadview"
+course="python"
+fees=5000
+print('institute = %s course = %s and fees is = %d' %(s , course , fees))
+
+#Ques 6-
+name= "tony stark"
+salary= 10000000
+print('%s %d' %(name , salary))
